a92e084cf030e1e9da1ee9eb922f8cd24696705d
Vahan Nadjarian <vahan@nadjarian.com>
2023-06-22 09:55:44 -0400

    completed mock cc

--- server/app.py
+++ server/app.py
@@ -33,6 +33,113 @@ api = Api(app)
 def home():
     return ""
 
+class Customers(Resource):
+    def get(self):
+        try:
+            customers = [c.to_dict(only=("id", "name", "email")) for c in Customer.query.all()]
+            return customers, 200
+        except:
+            return {"error": "Bad request"}, 400
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_customer = Customer(
+                name = data.get('name'),
+                email = data.get('email')
+            )
+            db.session.add(new_customer)
+            db.session.commit()
+
+            return new_customer.to_dict(only=("id", "name", "email")), 201
+        except:
+            return {"error": "400: Validation error"}, 400
+
+api.add_resource(Customers, "/customers")
+
+
+class CustomersById(Resource):
+    def get(self, id):
+        try:
+            customer = (
+                Customer.query.filter(Customer.id == id)
+                .first().to_dict(only=("id", "name", "email", "reservations"))
+            )
+            return customer, 200
+        except:
+            {"error": "404: Customer not found"}, 404
+
+api.add_resource(CustomersById, "/customers/<int:id>")
+
+
+class Locations(Resource):
+    def get(self):
+        try:
+            locations = [
+                l.to_dict(only=("id", "name", "max_party_size"))
+                for l in Location.query.all()
+            ]
+            return locations, 200
+        except:
+            return {"error": "Bad request"}, 400
+
+api.add_resource(Locations, "/locations")
+
+
+class Reservations(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_reservation = Reservation(
+                reservation_date = datetime.datetime.strptime(data.get('reservation_date'), "%Y-%m-%d").date(),
+                customer_id = data.get('customer_id'),
+                location_id = data.get('location_id'),
+                party_size = data.get('party_size'),
+                party_name = data.get('party_name'),
+            )
+            db.session.add(new_reservation)
+            db.session.commit()
+
+            return new_reservation.to_dict(), 201
+        except:
+            {"error": "Unable to post reservation"}, 400
+
+api.add_resource(Reservations, "/reservations")
+
+
+class ReservationById(Resource):
+    def patch(self, id):
+        data = request.get_json()
+        reservation = Reservation.query.filter(Reservation.id == id).first()
+        if not reservation:
+            return {"error": "404 not found"}, 404
+        for attr in data:
+            if attr == "reservation_date":
+                setattr(
+                    reservation, 
+                    attr,
+                    datetime.datetime.strptime(
+                    data.get("reservation_date"), "%Y-%m-%d").date(),
+                )
+            else:
+                setattr(reservation, attr, data.get(attr))
+        try:
+            db.session.add(reservation)
+            db.session.commit()
+            return reservation.to_dict(), 201
+        except:
+            return ({"error": "Unable to update reservation"}, 400)
+        
+    def delete(self, id):
+        reservation = Reservation.query.filter(Reservation.id == id).first()
+        if not reservation:
+            return ({"error": "404 not found"}, 404)
+        db.session.delete(reservation)
+        db.session.commit()
+        return ({}, 204)
+
+api.add_resource(ReservationById, "/reservations/<int:id>")
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -18,19 +18,94 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer:
+class Customer(db.Model, SerializerMixin):
     __tablename__ = "customers"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+
+    reservations = db.relationship("Reservation", back_populates="customer")
+    locations = association_proxy("reservations", "location" )
+
+    serialize_rules = ("-reservations.customer",)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name or len(name) < 1:
+            raise ValueError("Invalid name")
+        return name
+    
+    @validates('email')
+    def validate_email(self, key, email):
+        if "@" not in email:
+            raise ValueError("Invalid email")
+        return email
 
 
-class Location:
+class Location(db.Model, SerializerMixin):
     __tablename__ = "locations"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    max_party_size = db.Column(db.Integer, nullable=False)
+
+    reservations = db.relationship("Reservation", back_populates="location")
+    customers = association_proxy("reservations", "customer")
+
+    serialize_rules = ("-reservations.location",)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name or len(name) < 1:
+            raise ValueError("Invalid name")
+        return name
+    
+    @validates('max_party_size')
+    def validate_max_party_size(self, key, max_party_size):
+        if not isinstance(max_party_size, int):
+            raise ValueError("Invalid max_party_size")
+        return max_party_size
 
 
-class Reservation:
+
+class Reservation(db.Model, SerializerMixin):
     __tablename__ = "reservations"
 
+    __table_args__ = (UniqueConstraint("location_id", "customer_id", "reservation_date"),)
+
     id = db.Column(db.Integer, primary_key=True)
+    party_name = db.Column(db.String, nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
+    party_size = db.Column(db.Integer, nullable=False)
+    reservation_date = db.Column(db.Date, nullable=False)
+
+    customer = db.relationship("Customer", back_populates="reservations")
+    location = db.relationship("Location", back_populates="reservations")
+
+    serialize_rules = ("-location.reservations", "-customer.reservations")
+
+    @validates('party_name')
+    def validate_party_name(self, key, party_name):
+        if not party_name or len(party_name) < 1:
+            raise ValueError("Invalid party name")
+        return party_name
+    
+    @validates('customer_id')
+    def validate_customer_id(self, key, customer_id):
+        if not customer_id or not isinstance(customer_id, int):
+            raise ValueError("Invalid customer_id")
+        return customer_id
+    
+    @validates('location_id')
+    def validate_location_id(self, key, location_id):
+        if not location_id or not isinstance(location_id, int):
+            raise ValueError("Invalid location_id")
+        return location_id
+        
+    @validates('reservation_date')
+    def validate_reservation_date(self, key, reservation_date):
+        if not isinstance(reservation_date, datetime.date):
+            raise TypeError("Invalid reservation_date")
+        return reservation_date
